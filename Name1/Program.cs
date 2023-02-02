8f96a430600f217536c8e77d636931350d5e44ca
Elena Poganova <a.n.rus@mail.ru>
2023-02-02 23:23:14 +0400

    Name1

---
+++ Name1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int[]array = new int [5];
+for (int i=0; i<array.Length; i++)
+array[i] = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+    summa++;
+    }
+}
+
