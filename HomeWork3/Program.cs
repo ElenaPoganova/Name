9e50b51057221218b74d18429d129a9ccbb144e6
Elena Poganova <a.n.rus@mail.ru>
2023-02-06 22:01:03 +0400

    HomeWork3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,45 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    int summa = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        summa = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            summa += matrix[i, j];
+        }
+        Console.WriteLine($"Результат: {summa / matrix.GetLength(0)}");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив:");
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
