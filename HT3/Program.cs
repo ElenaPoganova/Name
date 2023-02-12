918c96871efae819c82a370be6b902375c08e38a
Elena Poganova <a.n.rus@mail.ru>
2023-02-12 18:46:36 +0400

    HT3

---
+++ HT3/Program.cs
@@ -0,0 +1,47 @@
+﻿/* 58 Две матрицы. найти произведение */
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11);
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+            Console.WriteLine();
+    }
+}
+
+
+int[,] ReleaseMatrix(int[,] FirstMatrix, int[,] SecondMatrix)
+{
+    int[,] result = new int[FirstMatrix.GetLength(0), FirstMatrix.GetLength(1)];
+    for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+            result[i, j] = FirstMatrix[i, j] * SecondMatrix[i, j];
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите размеры массивов: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] FirstMatrix = new int[size[0], size[1]];
+int[,] SecondMatrix = new int[size[0], size[1]];
+InputMatrix(FirstMatrix);
+InputMatrix(SecondMatrix);
+Console.WriteLine("1-й начальный массив: ");
+PrintMatrix(FirstMatrix);
+Console.WriteLine("2-й начальный массив: ");
+PrintMatrix(SecondMatrix);
+Console.WriteLine("Результат произведения:");
+PrintMatrix(ReleaseMatrix(FirstMatrix, SecondMatrix));
